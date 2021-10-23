f271049843888d34c0b87feaded147a131c0f1d7
H-Soba Vincent <hsobav@gmail.com>
2021-10-22 23:17:07 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        double value = 0;
+        string operation = "";
+        bool opSelected = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            if ((displayResult.Text == "0") || opSelected)
+            {
+                displayResult.Clear();
+            }
+            opSelected = false;
+            Button b = (Button)sender;
+            displayResult.Text += b.Content;
+        }
+
+        private void btnClearEntry_Click(object sender, RoutedEventArgs e)
+        {
+            displayResult.Text = "0";
+        }
+
+        private void btnOperator_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            operation = (string)b.Content;
+            value = double.Parse(displayResult.Text);
+            equation.Content = value + " " + operation;
+        }
+
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "/";
+            value = double.Parse(displayResult.Text);
+            opSelected = true;
+            equation.Content = value + " " + operation;
+        }
+
+        private void btnMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "*";
+            value = double.Parse(displayResult.Text);
+            opSelected = true;
+            equation.Content = value + " " + operation;
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "-";
+            value = double.Parse(displayResult.Text);
+            opSelected = true;
+            equation.Content = value + " " + operation;
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "+";
+            value = double.Parse(displayResult.Text);
+            opSelected = true;
+            equation.Content = value + " " + operation;
+        }
+
+        private void btnEquals_Click(object sender, RoutedEventArgs e)
+        {
+            equation.Content = "";
+            switch (operation)
+            {
+                case "+":
+                    displayResult.Text = (value + double.Parse(displayResult.Text)).ToString();
+                    break;
+                case "-":
+                    displayResult.Text = (value - double.Parse(displayResult.Text)).ToString();
+                    break;
+                case "*":
+                    displayResult.Text = (value * double.Parse(displayResult.Text)).ToString();
+                    break;
+                case "/":
+                    displayResult.Text = (value / double.Parse(displayResult.Text)).ToString();
+                    break;
+            }
+        }
+
+        private void btnPlusMinus_Click(object sender, RoutedEventArgs e)
+        {
+            displayResult.Text = (-(double.Parse(displayResult.Text))).ToString();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            displayResult.Clear();
+            value = 0;
+        }
+
+        private void btnBackspace_Click(object sender, RoutedEventArgs e)
+        {
+            if (displayResult.Text.Length > 0)
+            {
+                displayResult.Text = displayResult.Text.Remove(displayResult.Text.Length - 1);
+            }
+        }
+    }
+}
